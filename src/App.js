4473068a3d5d416dcf018ffe8109dda0881e580b
zschultz00 <112969769+zschultz00@users.noreply.github.com>
2023-04-16 03:43:07 -0400

    save

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nform from "./Nform";
-import { Container, Row, Col, Nav, Navbar, Card, Form, Button} from 'react-bootstrap';
-
+import { Container, Row, Col, Nav, Navbar, Card, Image} from 'react-bootstrap';
 import React, { useState, useEffect } from 'react';
 
 
@@ -43,11 +42,230 @@ function DogImage() {
 
 //end chatgpt
 
+//written by zach
 function RandomPetImage() {
   const isDog = Math.random() < 0.5;
 
   return isDog ? DogImage() : CatImage();
 }
+
+//end written by zach
+
+//chatgpt write a react function with pet descriptions. I modified it because it didn't work initially -- Zach
+function PetDesc() {
+  const PetDescription = () => {
+    const petDescriptions = [
+      "Fluffy and cuddly",
+      "Playful and energetic",
+      "Intelligent and curious",
+      "Loyal and protective",
+      "Graceful and agile",
+      "Affectionate and loving",
+      "Independent and self-sufficient",
+      "Adventurous and explorative",
+      "Eager to please and obedient",
+      "Majestic and regal",
+      "Mischievous and playful",
+      "Laid-back and relaxed",
+      "Gentle and calm",
+      "Fearless and brave",
+      "Elegant and poised",
+      "Charming and charismatic",
+      "Alert and vigilant",
+      "Friendly and social",
+      "Watchful and attentive",
+      "Adaptable and versatile",
+      "Sleek and swift",
+      "Proud and confident",
+      "Courageous and strong-willed",
+      "Energetic and enthusiastic",
+      "Inquisitive and investigative",
+      "Expressive and communicative",
+      "Patient and tolerant",
+      "Protective and nurturing",
+      "Gracious and dignified",
+      "Playful and silly",
+      "Intense and focused"
+    ];
+
+    const randomDescription = petDescriptions[Math.floor(Math.random() * petDescriptions.length)];
+
+    return (
+      <p>Description: "{randomDescription}" -- ChatGPT</p>
+    );
+  };
+
+  return (
+    <div>
+      <PetDescription />
+    </div>
+  );
+}
+
+
+//chatgpt based on the function above. I asked Chatgpt to make a javascript array of pet names for cats or dogs. i inputted that array into here.
+function PetName() {
+  const PetDescription = () => {
+    const petDescriptions = [
+        "Buddy",
+        "Max",
+        "Charlie",
+        "Lucy",
+        "Daisy",
+        "Sadie",
+        "Molly",
+        "Bailey",
+        "Lola",
+        "Rocky",
+        "Tucker",
+        "Jack",
+        "Milo",
+        "Bella",
+        "Cooper",
+        "Riley",
+        "Zoe",
+        "Bear",
+        "Ruby",
+        "Lily",
+        "Harley",
+        "Roxy",
+        "Leo",
+        "Maggie",
+        "Chloe",
+        "Gus",
+        "Luna",
+        "Sasha",
+        "Rosie",
+        "Jasper",
+        "Oscar",
+        "Finn",
+        "Rusty",
+        "Penny",
+        "Sammy",
+        "Coco",
+        "Winston",
+        "Mickey",
+        "Mimi",
+        "Simba",
+        "Oliver",
+        "George",
+        "Mia",
+        "Willow",
+        "Lucky",
+        "Ginger",
+        "Diesel",
+        "Hazel",
+        "Phoebe",
+        "Marley",
+        "Lenny",
+        "Milo",
+        "Frankie",
+        "Ella",
+        "Cleo",
+        "Duke",
+        "Ruby",
+        "Zeus",
+        "Smokey",
+        "Sophie",
+        "Toby",
+        "Buster",
+        "Scout",
+        "Nala",
+        "Hank",
+        "Bentley",
+        "Cali",
+        "Ranger",
+        "Lacey",
+        "Chico",
+        "Izzy",
+        "Maverick",
+        "Piper",
+        "Stella",
+        "Auggie",
+        "Benny",
+        "Peanut",
+        "Hudson",
+        "Josie",
+        "Maya",
+        "Hercules",
+        "Zelda",
+        "Roscoe",
+        "Bo",
+        "Louie",
+        "Sunny",
+        "Rex",
+        "Ellie",
+        "Rocco",
+        "Lulu",
+        "Abby",
+        "Brady",
+        "Cody",
+        "Henry",
+        "Ace",
+        "Blue",
+        "Trixie",
+        "Bandit",
+        "Boris",
+        "Bruno",
+        "Cassie",
+        "Cinnamon",
+        "Dakota",
+        "Dexter",
+        "Emmie",
+        "Fergus",
+        "Gracie",
+        "Gretchen",
+        "Hunter",
+        "Indy",
+        "Joey",
+        "Koda",
+        "Kona",
+        "Laddie",
+        "Lexi",
+        "Logan",
+        "Lola",
+        "Mac",
+        "Macy",
+        "Maggy",
+        "Murphy",
+        "Nikki",
+        "Oakley",
+        "Olive",
+        "Onyx",
+        "Pepper",
+        "Rascal",
+        "Sandy",
+        "Snickers",
+        "Sully",
+        "Sydney",
+        "Taz",
+        "Teddy",
+        "Tessa",
+        "Thor",
+        "Wally",
+        "Winnie",
+        "Yogi",
+        "Ziggy",
+        "Zoey"
+      ];
+      
+
+    const randomDescription = petDescriptions[Math.floor(Math.random() * petDescriptions.length)];
+
+    return (
+      <p>Name: "{randomDescription}" -- ChatGPT</p>
+    );
+  };
+
+  return (
+    <div>
+      <PetDescription />
+    </div>
+  );
+}
+
+//end chatgpt
+
 
 function CardPet() {
 
@@ -57,10 +275,9 @@ function CardPet() {
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={petUrl} />
       <Card.Body>
-        <Card.Title>Pet</Card.Title>
+        <Card.Title><PetName/></Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          <PetDesc/>
         </Card.Text>
       </Card.Body>
     </Card>
@@ -86,43 +303,7 @@ function App() {
           </Navbar>
         </Col>
       </Row>
-      <Row className="mt-5 pt-3">
-        <Col className="p-3" sm={12} md={6} lg={6} xl={3}>
-          <CardPet/>
-        </Col>
-        <Col className="p-3" sm={12} md={6} lg={6} xl={3}>
-          <CardPet/>
-        </Col>
-        <Col className="p-3" sm={12} md={6} lg={6} xl={3}>
-          <CardPet/>
-        </Col>
-        <Col className="p-3" sm={12} md={6} lg={6} xl={3}>
-          <CardPet/>
-        </Col>
-        <Col className="p-3" sm={12} md={6} lg={6} xl={3}>
-          <CardPet/>
-        </Col>
-        <Col className="p-3" sm={12} md={6} lg={6} xl={3}>
-          <CardPet/>
-        </Col>
-        <Col className="p-3" sm={12} md={6} lg={6} xl={3}>
-          <CardPet/>
-        </Col>
-        <Col className="p-3" sm={12} md={6} lg={6} xl={3}>
-          <CardPet/>
-        </Col>
-        <Col className="p-3" sm={12} md={6} lg={6} xl={3}>
-          <CardPet/>
-        </Col>
-        <Col className="p-3" sm={12} md={6} lg={6} xl={3}>
-          <CardPet/>
-        </Col>
-        <Col className="p-3" sm={12} md={6} lg={6} xl={3}>
-          <CardPet/>
-        </Col>
-        <Col className="p-3" sm={12} md={6} lg={6} xl={3}>
-          <CardPet/>
-        </Col>
+      <Row className="mt-5 pt-3 ">
         <Col className="p-3" sm={12} md={6} lg={6} xl={3}>
           <CardPet/>
         </Col>
