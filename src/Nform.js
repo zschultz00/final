f0076f58557535cc96d8ac5bf645a8650795a02d
zschultz00 <112969769+zschultz00@users.noreply.github.com>
2023-04-16 00:59:46 -0400

    form things

--- src/Nform.js
+++ src/Nform.js
@@ -1,28 +1,26 @@
 import React from "react";
-import { Container, Row, Col, Nav, Navbar, Card, Form, Button} from 'react-bootstrap';
 
 export default function Nform() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-
+    <form name="contact" method="post">
+      <div class="form-group">
+        <input type="hidden" name="form-name" value="contact" />
+        <div class="form-group">
+          <label htmlFor="name">Name</label> <br />
+          <input class="form-control" type="text" id="name" name="name" required />
+        </div>
+        <div class="form-group">
+          <label htmlFor="email">Email</label> <br />
+          <input class="form-control" type="email" id="email" name="email" required />
+        </div>
+        <div class="form-group">
+          <label htmlFor="message">Message</label> <br />
+          <textarea class="form-control" id="message" name="message" required></textarea>
+        </div>
+        <div class="form-group">
+          <input class="btn btn-primary mt-2" type="submit" value="Submit message" />
+        </div>
+      </div>
+    </form>
   );
 }
